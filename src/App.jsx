18ab5b5175c4f0d48e6dcc8aac714462d38b4eaa
moyo <moyosxtouch@gmail.com>
2024-04-22 11:50:29 -0600

    fin del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 function App() {
-  const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
   const MAX_ITEMS = 5;
   const MIN_ITEMS = 1;
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
     if (itemExists >= 0) {
